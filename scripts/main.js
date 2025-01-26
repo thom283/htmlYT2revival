cd21bd2d9ca2f38539b18a6996e0ade13f856708
ququnta <filimonov3737@ya.ru>
2025-01-26 16:03:26 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
+//settings
+let glype = localStorage.getItem('glype');
+let invidious = localStorage.getItem('invidious');
+if (!glype) {
+	localStorage.setItem('glype', 'https://nnp.nnchan.ru/glype/browse.php?u=');
+	glype = localStorage.getItem('glype');
+}
+if (!invidious) {
+	localStorage.setItem('invidious', 'https://inv.nadeko.net/');
+	invidious = localStorage.getItem('invidious');
+}
+
+//per page fns
 function trends(region) {
 	//alert(region);
 }
@@ -16,6 +29,18 @@ function channel() {
 }
 function playlist() {
 	let playlistId = getParameterByName('playlistId');
+}
+function showPopup() {
+	let popup = document.getElementById('popup');
+	let wrapper = document.getElementById('wrapper');
+	popup.style.display = "block";
+	wrapper.style.display = "none";
+}
+function hidePopup() {
+	let popup = document.getElementById('popup');
+	let wrapper = document.getElementById('wrapper');
+	wrapper.style.display = "block";
+	popup.style.display = "none";
 }
 
 //stealed scripts
@@ -47,3 +72,21 @@ function openTab(evt, tabName) {
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
 }
+
+//spoiler
+window.acc=(e)=>{
+	let q=document.querySelectorAll(".acc"),
+	w=document.querySelectorAll(".accD");
+	q.forEach((e2,l)=>{
+		if(e==e2){
+			if(!e.classList.contains("accCl")){
+				e.classList.add("accCl");
+				w[l].classList.remove("dn")
+			}else{
+				e.classList.remove("accCl");
+				w[l].classList.add("dn")
+			}
+		}
+
+	})
+};
