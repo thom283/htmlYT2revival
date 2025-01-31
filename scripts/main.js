67d633acd8ca2c7c3622159054e31053ce5e5669
ququnta <filimonov3737@ya.ru>
2025-01-31 17:00:09 +0300

    old browser possibly fixes

--- scripts/main.js
+++ scripts/main.js
@@ -379,47 +379,46 @@ document.addEventListener('DOMContentLoaded', displayBookmarks);
 
 //stolen scripts
 
-//urlparser
-function getParameterByName(name, url = window.location.href) {
-	name = name.replace(/[\[\]]/g, '\\$&');
-	var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-	results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, ' '));
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
 //tab manager
 function openTab(evt, tabName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
 //spoiler
-window.acc=(e)=>{
-	let q=document.querySelectorAll(".acc"),
-	w=document.querySelectorAll(".accD");
-	q.forEach((e2,l)=>{
-		if(e==e2){
-			if(!e.classList.contains("accCl")){
-				e.classList.add("accCl");
-				w[l].classList.remove("dn")
-			}else{
-				e.classList.remove("accCl");
-				w[l].classList.add("dn")
-			}
-		}
-
-	})
+window.acc = function(e) {
+    var q = document.querySelectorAll(".acc"),
+        w = document.querySelectorAll(".accD");
+    Array.prototype.forEach.call(q, function(e2, l) {
+        if (e == e2) {
+            if (!e.classList.contains("accCl")) {
+                e.classList.add("accCl");
+                w[l].classList.remove("dn");
+            } else {
+                e.classList.remove("accCl");
+                w[l].classList.add("dn");
+            }
+        }
+    });
 };
 
 //duration
