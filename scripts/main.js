21bb1ed86d8e38363c0fa70e671b930f6b5e8dda
ququnta <filimonov3737@ya.ru>
2025-01-31 17:40:18 +0300

    fix bookmarks

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 //settings
-let glype = localStorage.getItem('glype');
-let apikey = localStorage.getItem('key');
-let bookmarks = localStorage.getItem('bookmarks');
+var glype = localStorage.getItem('glype');
+var apikey = localStorage.getItem('key');
+var bookmarks = localStorage.getItem('bookmarks');
 if (!glype) {
 	localStorage.setItem('glype', 'https://api.codetabs.com/v1/proxy/?quest=');
 	glype = localStorage.getItem('glype');
@@ -11,28 +11,28 @@ if (!apikey) {
 	apikey = localStorage.getItem('key');
 }
 //definitions
-let pageToken = '';
-let page = 0;
-let playlist_token = '';
-let playlist_list_token = '';
+var pageToken = '';
+var page = 0;
+var playlist_token = '';
+var playlist_list_token = '';
 
 //per page functions
 function trends(region) {
-	let xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=15&regionCode='+region+'&key='+apikey, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
-		let videolist = document.getElementById('videobits');
+		var response = xhr.response;
+		var videolist = document.getElementById('videobits');
 		response.items.forEach(function(item) {
-			let id = item.id;
-			let date = timeSince(item.snippet.publishedAt);
-			let title = item.snippet.title;
-			let thumb = item.snippet.thumbnails.default.url;
-			let channelTitle = item.snippet.channelTitle;
-			let duration = isoDurationToHHMMSS(item.contentDetails.duration);
-			let views = item.statistics.viewCount;
+			var id = item.id;
+			var date = timeSince(item.snippet.publishedAt);
+			var title = item.snippet.title;
+			var thumb = item.snippet.thumbnails.default.url;
+			var channelTitle = item.snippet.channelTitle;
+			var duration = isoDurationToHHMMSS(item.contentDetails.duration);
+			var views = item.statistics.viewCount;
 			videolist.insertAdjacentHTML('beforeend', `
 			<!--bit start-->
 				<div class="videobit">
@@ -55,24 +55,24 @@ function trends(region) {
 	}
 }
 function video() {
-	let videoid = getParameterByName('id');
-	let xhr = new XMLHttpRequest();
+	var videoid = getParameterByName('id');
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id='+videoid+'&key='+apikey, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
+		var response = xhr.response;
 		console.log(response);
-		let vidTitle = document.getElementById('vidTitle');
-		let vidDate = document.getElementById('vidDate');
-		let viewsLikes = document.getElementById('viewsLikes');
-		let vidChannel = document.getElementById('vidChannel');
-		let vidDescription = document.getElementById('vidDescription');
+		var vidTitle = document.getElementById('vidTitle');
+		var vidDate = document.getElementById('vidDate');
+		var viewsLikes = document.getElementById('viewsLikes');
+		var vidChannel = document.getElementById('vidChannel');
+		var vidDescription = document.getElementById('vidDescription');
 		vidTitle.innerHTML = response.items[0].snippet.title;
 		vidDate.innerHTML = timeSince(response.items[0].snippet.publishedAt);
 		viewsLikes.innerHTML = response.items[0].statistics.viewCount+' views, '+response.items[0].statistics.likeCount+' likes';
-		let chanId = response.items[0].snippet.channelId;
-		let chanTitle = response.items[0].snippet.channelTitle;
+		var chanId = response.items[0].snippet.channelId;
+		var chanTitle = response.items[0].snippet.channelTitle;
 		vidChannel.innerHTML = `<a class="button" href="channel.html?channelId=${chanId}">${chanTitle}</a>`;
 		vidDescription.innerHTML = response.items[0].snippet.description.replace('\n', '<br />');
 		document.getElementById('bmarkcon').innerHTML = `<button class="favBtn btnToolbar" onclick="toggleBookmark('${videoid}', '${response.items[0].snippet.title}', '${response.items[0].snippet.thumbnails.default.url}')"></button>`;
@@ -80,36 +80,36 @@ function video() {
 	}
 }
 function search() {
-	let videolist = document.getElementById('videobits');
+	var videolist = document.getElementById('videobits');
 	page+=1;
-	let query = getParameterByName('searchField');
-	let sfld = document.getElementById('sfld');
+	var query = getParameterByName('searchField');
+	var sfld = document.getElementById('sfld');
 	sfld.value = query;
-	let sort = getParameterByName('sort');
-	let encquery = encodeURIComponent(query);
-	let containerQuery = document.getElementById('containerQuery');
+	var sort = getParameterByName('sort');
+	var encquery = encodeURIComponent(query);
+	var containerQuery = document.getElementById('containerQuery');
 	containerQuery.innerHTML = query;
 	//кино на время говнокодинга https://ok.ru/video/1367341533919?fromTime=3408
-	let url = 'https://www.googleapis.com/youtube/v3/search?part=id,snippet&maxResults=25&type=video&q='+encquery+'&order='+sort+'&key='+apikey;
+	var url = 'https://www.googleapis.com/youtube/v3/search?part=id,snippet&maxResults=25&type=video&q='+encquery+'&order='+sort+'&key='+apikey;
 	if (!!pageToken) {
 		url+='&pageToken='+pageToken;
 	}
 	videolist.insertAdjacentHTML('beforeend', `
 	<p>Page: ${page}</p>
 	`);
-	let xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', url, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
+		var response = xhr.response;
 		
 		response.items.forEach(function(item) {
-			let id = item.id.videoId;
-			let date = timeSince(item.snippet.publishedAt);
-			let title = item.snippet.title;
-			let thumb = item.snippet.thumbnails.default.url;
-			let channelTitle = item.snippet.channelTitle;
+			var id = item.id.videoId;
+			var date = timeSince(item.snippet.publishedAt);
+			var title = item.snippet.title;
+			var thumb = item.snippet.thumbnails.default.url;
+			var channelTitle = item.snippet.channelTitle;
 			videolist.insertAdjacentHTML('beforeend', `
 			<!--bit start-->
 				<div class="videobit">
@@ -133,27 +133,27 @@ function search() {
 	}
 }
 function channel_search(channnelSearchId) {
-	let videolist = document.getElementById('videobits');
+	var videolist = document.getElementById('videobits');
 	page+=1;	
-	let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet,id&order=date&maxResults=20&channelId='+channnelSearchId+'&key='+apikey;
+	var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet,id&order=date&maxResults=20&channelId='+channnelSearchId+'&key='+apikey;
 	if (!!pageToken) {
 		url+='&pageToken='+pageToken;
 	}
 	videolist.insertAdjacentHTML('beforeend', `
 	<p>Page: ${page}</p>
 	`);
-	let xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', url, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
+		var response = xhr.response;
 		response.items.forEach(function(item) {
-			let id = item.id.videoId;
-			let date = timeSince(item.snippet.publishedAt);
-			let title = item.snippet.title;
-			let thumb = item.snippet.thumbnails.default.url;
-			let channelTitle = item.snippet.channelTitle;
+			var id = item.id.videoId;
+			var date = timeSince(item.snippet.publishedAt);
+			var title = item.snippet.title;
+			var thumb = item.snippet.thumbnails.default.url;
+			var channelTitle = item.snippet.channelTitle;
 			videolist.insertAdjacentHTML('beforeend', `
 			<!--bit start-->
 				<div class="videobit">
@@ -177,17 +177,17 @@ function channel_search(channnelSearchId) {
 	}
 }
 function comments(vid){
-	let commentContainer = document.getElementById('comments');
-	let xhr = new XMLHttpRequest();
+	var commentContainer = document.getElementById('comments');
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', 'https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=50&videoId='+vid+'&key='+apikey, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
+		var response = xhr.response;
 		response.items.forEach(function(item) {
-			let date = timeSince(item.snippet.topLevelComment.snippet.publishedAt);
-			let name = item.snippet.topLevelComment.snippet.authorDisplayName;
-			let text = item.snippet.topLevelComment.snippet.textDisplay.replace('\n', '<br />');
+			var date = timeSince(item.snippet.topLevelComment.snippet.publishedAt);
+			var name = item.snippet.topLevelComment.snippet.authorDisplayName;
+			var text = item.snippet.topLevelComment.snippet.textDisplay.replace('\n', '<br />');
 			commentContainer.insertAdjacentHTML('beforeend', `
 			  <div class="comments">
 				<h4>${name}</h4>
@@ -200,21 +200,21 @@ function comments(vid){
 }
 function channel() {
 	//main info
-	let channelId = getParameterByName('channelId');
-	let xhr = new XMLHttpRequest();
+	var channelId = getParameterByName('channelId');
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', 'https://www.googleapis.com/youtube/v3/channels?part=id,snippet,statistics&id='+channelId+'&key='+apikey, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
-		let chanUsername = document.getElementById('chanUsername');
-		let chanPic = document.getElementById('chanPic');
-		let chanViews = document.getElementById('chanViews');
-		let chanUploaded = document.getElementById('chanUploaded');
-		let chanCustomName = document.getElementById('chanCustomName');
-		let chanRegion = document.getElementById('chanRegion');
-		let chanDescription = document.getElementById('chanDescription');
-		let chanSubscribers = document.getElementById('chanSubscribers');
+		var response = xhr.response;
+		var chanUsername = document.getElementById('chanUsername');
+		var chanPic = document.getElementById('chanPic');
+		var chanViews = document.getElementById('chanViews');
+		var chanUploaded = document.getElementById('chanUploaded');
+		var chanCustomName = document.getElementById('chanCustomName');
+		var chanRegion = document.getElementById('chanRegion');
+		var chanDescription = document.getElementById('chanDescription');
+		var chanSubscribers = document.getElementById('chanSubscribers');
 		chanUsername.innerHTML = response.items[0].snippet.title;
 		chanViews.innerHTML = response.items[0].statistics.viewCount;
 		chanUploaded.innerHTML = response.items[0].statistics.videoCount;
@@ -229,21 +229,21 @@ function channel() {
 }
 
 function chanPlaylist() {
-	let channelId = getParameterByName('channelId');
-	let url = 'https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails,id&channelId='+channelId+'&maxResults=50&key='+apikey;
+	var channelId = getParameterByName('channelId');
+	var url = 'https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails,id&channelId='+channelId+'&maxResults=50&key='+apikey;
 	if (!!playlist_token){
 		url+='&pageToken='+playlist_token;
 	}
-	let xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', url, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
+		var response = xhr.response;
 		response.items.forEach(function(item) {
-			let playlist_container = document.getElementById('playlistbits');
-			let id = item.id;
-			let title = item.snippet.title;
+			var playlist_container = document.getElementById('playlistbits');
+			var id = item.id;
+			var title = item.snippet.title;
 			playlist_container.insertAdjacentHTML('beforeend', `
 			<li class="playlistbit"><a href="playlist.html?playlistId=${id}">${title}</a></li>
 			`);
@@ -254,40 +254,40 @@ function chanPlaylist() {
 }
 
 function playlist() {
-	let playlistId = getParameterByName('playlistId');
+	var playlistId = getParameterByName('playlistId');
 	//playlist metadata
-	let xhr_meta = new XMLHttpRequest();
+	var xhr_meta = new XMLHttpRequest();
 	xhr_meta.responseType = 'json';
 	xhr_meta.open('GET', 'https://www.googleapis.com/youtube/v3/playlists?part=snippet&id='+playlistId+'&key='+apikey, true);
 	xhr_meta.send();
 	xhr_meta.onload = function() {
-		let response = xhr_meta.response;
+		var response = xhr_meta.response;
 		document.getElementById('playlist_title').innerHTML = response.items[0].snippet.title;
 		document.getElementById('playlist_description').innerHTML = response.items[0].snippet.description;
 		document.getElementById('playlist_author').innerHTML = 'by '+response.items[0].snippet.channelTitle;
 	} 
 	//view items of playlist
-	let url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=id%2Csnippet%2CcontentDetails&playlistId='+playlistId+'&key='+apikey;
+	var url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=id%2Csnippet%2CcontentDetails&playlistId='+playlistId+'&key='+apikey;
 	if (!!playlist_list_token){
 		url+='&pageToken='+playlist_list_token;
 	}
-	let videolist = document.getElementById('videobits');
+	var videolist = document.getElementById('videobits');
 	page+=1;	
 	videolist.insertAdjacentHTML('beforeend', `
 		<p>Page: ${page}</p>
 		`);
-	let xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
 	xhr.open('GET', url, true);
 	xhr.send();
 	xhr.onload = function() {
-		let response = xhr.response;
+		var response = xhr.response;
 		response.items.forEach(function(item) {
-			let id = item.snippet.resourceId.videoId;
-			let date = timeSince(item.snippet.publishedAt);
-			let title = item.snippet.title;
-			let thumb = item.snippet.thumbnails.default.url;
-			let channelTitle = item.snippet.channelTitle;
+			var id = item.snippet.resourceId.videoId;
+			var date = timeSince(item.snippet.publishedAt);
+			var title = item.snippet.title;
+			var thumb = item.snippet.thumbnails.default.url;
+			var channelTitle = item.snippet.channelTitle;
 			videolist.insertAdjacentHTML('beforeend', `
 			<!--bit start-->
 				<div class="videobit">
@@ -311,25 +311,25 @@ function playlist() {
 	}
 }
 function showPopup() {
-	let popup = document.getElementById('popup');
-	let wrapper = document.getElementById('wrapper');
+	var popup = document.getElementById('popup');
+	var wrapper = document.getElementById('wrapper');
 	popup.style.display = "block";
 	wrapper.style.display = "none";
 }
 function hidePopup() {
-	let popup = document.getElementById('popup');
-	let wrapper = document.getElementById('wrapper');
+	var popup = document.getElementById('popup');
+	var wrapper = document.getElementById('wrapper');
 	wrapper.style.display = "block";
 	popup.style.display = "none";
 }
 function shareVideo() {
-	let videoid = getParameterByName('id');
+	var videoid = getParameterByName('id');
 	
-	let data = {title: 'YT Link', url: 'https://www.youtube.com/watch?v='+videoid};
+	var data = {title: 'YT Link', url: 'https://www.youtube.com/watch?v='+videoid};
 	navigator.share(data);
 }
 function toggleBookmark(videoId, title, img) {
-    let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
     const bookmark = { videoId, title, img };
     const index = bookmarks.findIndex(b => b.videoId === videoId);
     if (index !== -1) {
@@ -375,6 +375,7 @@ function displayBookmarks() {
 
 
 
+
 //stolen scripts
 
 //urlparser
@@ -404,7 +405,7 @@ function openTab(evt, tabName) {
 
 //spoiler
 window.acc=(e)=>{
-	let q=document.querySelectorAll(".acc"),
+	var q=document.querySelectorAll(".acc"),
 	w=document.querySelectorAll(".accD");
 	q.forEach((e2,l)=>{
 		if(e==e2){
@@ -427,9 +428,9 @@ function isoDurationToHHMMSS(duration) {
 	if (!matches) {
 		throw new Error("Invalid ISO 8601 duration format");
 	}
-	let hours = parseInt(matches[1] || 0, 10);
-	let minutes = parseInt(matches[2] || 0, 10);
-	let seconds = parseInt(matches[3] || 0, 10);
+	var hours = parseInt(matches[1] || 0, 10);
+	var minutes = parseInt(matches[2] || 0, 10);
+	var seconds = parseInt(matches[3] || 0, 10);
 	const pad = (num) => String(num).padStart(2, '0');
 	return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
 }
